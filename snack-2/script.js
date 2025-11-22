9edf68399c92881aceb7c64a2054e6707907c8fa
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-22 13:50:54 +0100

    snack 2 completato

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,22 @@
 const people = [
-  { name: 'Paolo', age: 35 },
-  { name: 'Giulia', age: 24 },
-  { name: 'Marco', age: 67 }
+  { name: "Paolo", age: 35 },
+  { name: "Giulia", age: 24 },
+  { name: "Marco", age: 67 },
 ];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+// with for loop
+for (let i = 0; i < people.length; i++) {
+  const person = people[i];
+  console.log(person.name);
+}
+
+// with forEach
+people.forEach(person => console.log(person.name));
+
+// with for of
+for (const person of people) {
+  console.log(person.name)
+};
